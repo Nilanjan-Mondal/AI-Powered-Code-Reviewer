c0d856d56cd8cbd0a0496c8de4c45cd12d14879a
Nilanjan Mondal <nilanjanmondal165@gmail.com>
2025-03-10 16:18:02 +0530

    Added cors policy

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors')
 const aiRoutes = require('./routes/ai.routes');
 
 const ServerConfig = require('./config/serverConfig');
 // const connectDB = require('./config/dbConfig');
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.text());
